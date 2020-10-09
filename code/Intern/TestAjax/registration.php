143db9bc2a4a10f61d83e1929c8fa9d230ffe549
HiepPB <phamhiep03061999@gmai.com>
2020-10-09 17:11:45 +0700

    Upload folder code

--- code/Intern/TestAjax/registration.php
+++ code/Intern/TestAjax/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'TestJS_JS',
+    'Intern_TestAjax',
     __DIR__
 );
