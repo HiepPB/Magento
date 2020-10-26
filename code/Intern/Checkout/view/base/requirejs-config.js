c206a7394eed8cc209a90973306e4afa69f09ae6
HiepPB <phamhiep03061999@gmai.com>
2020-10-26 09:37:02 +0700

    Upload folder checkout

---
+++ code/Intern/Checkout/view/base/requirejs-config.js
@@ -0,0 +1,14 @@
+/*
+const config = {
+    'config': {
+        'mixins': {
+            'Magento_Checkout/js/view/shipping': {
+                'Intern_Checkout/js/view/shipping-payment-mixin': true
+            },
+            'Magento_Checkout/js/view/payment': {
+                'Intern_Checkout/js/view/shipping-payment-mixin': true
+            }
+        }
+    }
+};
+*/
